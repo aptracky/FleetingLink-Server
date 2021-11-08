4553c7fff69790f820bec6f7f91f59a14a3dcea4
Andrew Peterson <apeterson247@gmail.com>
2021-11-07 20:48:44 -0500

    deployment chnages

--- client/client/src/components/create.js
+++ client/client/src/components/create.js
@@ -1,67 +1,71 @@
-import * as React from 'react';
-import styles from '../components/create.module.css';
-import axios from 'axios'; 
-import logo from '../Logo.svg';
-
+import * as React from "react";
+import styles from "../components/create.module.css";
+import axios from "axios";
+import logo from "../Logo.svg";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/url/shorten'
-})
+    baseURL: "http://localhost:5000/api/url/shorten",
+});
 
 class Create extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            longUrl: '',
-            shortUrl: '',
-            urlCode: '',
+            longUrl: "",
+            shortUrl: "",
+            urlCode: "",
             err: false,
-            errMessage: ''
+            errMessage: "",
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange = event =>{
+    handleChange = (event) => {
         const target = event.target;
         const longUrl = target.longUrl;
         const urlCode = target.urlCode;
 
         this.setState({
             longUrl: longUrl,
-            urlCode: urlCode
+            urlCode: urlCode,
         });
-    }
+    };
 
-    /* This is where the magic happens 
-    */
-    handleSubmit = async event => {
+    /* This is where the magic happens
+     */
+    handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        await api.post('/', { longUrl: data.get('longUrl'), urlCode: data.get('urlCode') })
-        .then(res=>{
-            if(res.data.shortUrl == null) {
-                this.setState({ shortUrl: 'Error' })
-                console.log(this.state.shortUrl)
-            }
-            else {
-                this.setState({ shortUrl: res.data.shortUrl })
-                console.log(this.state.shortUrl)
-            }
-            this.setState.longUrl = '';
-        })
-        .catch(error => { 
-            console.error(error);
-            this.setState({ err: true });
-            this.setState({ errorMessage: error.message });
-        });
-    }
+        await api
+            .post("/", {
+                longUrl: data.get("longUrl"),
+                urlCode: data.get("urlCode"),
+            })
+            .then((res) => {
+                if (res.data.shortUrl == null) {
+                    this.setState({ shortUrl: "Error" });
+                    console.log(this.state.shortUrl);
+                } else {
+                    this.setState({ shortUrl: res.data.shortUrl });
+                    console.log(this.state.shortUrl);
+                }
+                this.setState.longUrl = "";
+            })
+            .catch((error) => {
+                console.error(error);
+                this.setState({ err: true });
+                this.setState({ errorMessage: error.message });
+            });
+    };
 
     render() {
         return (
             <div className={styles.container}>
-                <a href="https://fleetinglinks.com/"><img className={styles.logo} src={logo} alt="Logo"></img></a>
+                <a href="http://fleetinglink.com/">
+                    <img className={styles.logo} src={logo} alt="Logo"></img>
+                </a>
                 <div className={styles.navigation}>
                     <ul>
                         <li>Usage</li>
@@ -71,22 +75,38 @@ class Create extends React.Component {
                 </div>
                 <div className={styles.text}>Temporary and memorable links</div>
                 <div className={styles.borderBox} />
-                <div className={styles.cardBehind} /> 
+                <div className={styles.cardBehind} />
                 <div className={styles.card}>
                     <form onSubmit={this.handleSubmit}>
                         <div className={styles.formGroup}>
                             <label>
                                 PASTE LINK:
-                                <input name="longUrl" type="text" value={this.state.longUrl} onChange={this.handleChange} placeholder="www.url.com" />
+                                <input
+                                    name="longUrl"
+                                    type="text"
+                                    value={this.state.longUrl}
+                                    onChange={this.handleChange}
+                                    placeholder="www.url.com"
+                                />
                             </label>
                             <br />
                             <label>
                                 ENTER ALIAS:
-                                <input name="urlCode" type="text" value={this.state.urlCode} onChange={this.handleChange} placeholder="alias"/>
+                                <input
+                                    name="urlCode"
+                                    type="text"
+                                    value={this.state.urlCode}
+                                    onChange={this.handleChange}
+                                    placeholder="alias"
+                                />
                             </label>
                             <br />
-                            <input className={styles.submit} type="submit" value="Submit" /> 
                         </div>
+                        <input
+                            className={styles.submit}
+                            type="submit"
+                            value="Genrate Link"
+                        />
                     </form>
                 </div>
                 <div className={styles.backgroundTexture} />
